5b696051864cc4f2fa302f7aab7b1790cdb5a9c1
yeasin2002 <mdkawsarislam2002@gmail.com>
2025-10-20 12:42:12 +0600

    feat(routing): Add static params generation for locales
    - Implement generateStaticParams function to support static locale routing
    - Enables pre-rendering of pages for all configured locales

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -2,6 +2,7 @@ import { fontVariables } from "@/lib/fonts";
 import type { Metadata } from "next";
 import { NextIntlClientProvider } from "next-intl";
 import "./../globals.css";
+import { routing } from "@/i18n/routing";
 
 export const metadata: Metadata = {
   title: "ICSA",
@@ -20,4 +21,8 @@ export default function RootLayout({
       </body>
     </html>
   );
+}
+
+export async function generateStaticParams() {
+  return routing.locales.map((locale) => ({ locale }));
 }
